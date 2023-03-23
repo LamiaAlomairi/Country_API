319f338908a215db49f1bec1a543ee5626699350
LamiaAlomairi <lama5alomairi@gmail.com>
2023-03-23 13:55:56 +0400

    add some data fetching from api

--- src/Country/Fetch_Data.java
+++ src/Country/Fetch_Data.java
@@ -2,6 +2,7 @@ package Country;
 
 import java.io.*;
 import java.net.*;
+import java.util.List;
 
 import com.google.gson.*;
 
@@ -30,11 +31,14 @@ public class Fetch_Data extends MenuItem{
 
             JsonParser parser = new JsonParser();
             JsonArray jsonArray = parser.parse(response.toString()).getAsJsonArray(); // parse the response into a JsonArray
-            
+            System.out.printf("%-45s %-60s %-280s %-50s %-50s %-50s %-50s %-50s %-50s %-50s %-50s %-50s%n","common","official","nativeName", "tld", "cca2", "ccn3", "cca3",
+            		"cioc", "independent", "status", "unMember", "GTQ");
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
             for (int i = 0; i < jsonArray.size(); i++) {
                 JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
                 String common = jsonObject.getAsJsonObject("name").get("common").getAsString(); // extract the name
                 String official = jsonObject.getAsJsonObject("name").get("official").getAsString();
+                
                 JsonObject nativeNameObj = jsonObject.getAsJsonObject("name").getAsJsonObject("nativeName");
                 if(nativeNameObj != null){
                     JsonObject languageObj = nativeNameObj.getAsJsonObject("Language");
@@ -43,10 +47,42 @@ public class Fetch_Data extends MenuItem{
                         String commonn = languageObj.get("common").getAsString();
                     }
                 }
+                
+                JsonElement tldElement = jsonObject.get("tld");
+                JsonArray tld = tldElement != null ? tldElement.getAsJsonArray() : null;
+                JsonElement cca2Element = jsonObject.get("cca2");
+                String cca2 = cca2Element != null ? cca2Element.getAsString() : null;
+                JsonElement ccn3Element = jsonObject.get("ccn3");
+                int ccn3 = ccn3Element != null ? ccn3Element.getAsInt() : 0;
+                JsonElement cca3Element = jsonObject.get("cca3");
+                String cca3 = cca3Element != null ? cca3Element.getAsString() : null;
+                JsonElement ciocElement = jsonObject.get("cioc");
+                String cioc = ciocElement != null ? ciocElement.getAsString() : null;
+                JsonElement independentElement = jsonObject.get("independent");
+                boolean independent = independentElement != null ? independentElement.getAsBoolean() : false;
+                JsonElement statusElement = jsonObject.get("status");
+                String status = statusElement != null ? statusElement.getAsString() : null;
+                JsonElement unMemberElement = jsonObject.get("unMember");
+                boolean unMember = unMemberElement != null ? unMemberElement.getAsBoolean() : false;
+                
+                JsonObject GTQeObj = jsonObject.getAsJsonObject("currencies").getAsJsonObject("GTQ");
+                if (GTQeObj != null && !GTQeObj.isJsonNull()) { // Check if GTQeObj exists and is not null
+                	JsonElement nameeElement = GTQeObj.get("name");
+                    String namee = nameeElement != null ? ciocElement.getAsString() : null;
+                    JsonElement symbolElement = GTQeObj.get("symbol");
+                    if (symbolElement != null && !symbolElement.isJsonNull()) { // Check if symbolElement exists and is not null
+                        char symboll = symbolElement.getAsString().charAt(0);
+                        System.out.printf("%-50s %-50c%n", namee, symboll);
+                    }
+                } else {
+                    System.out.printf("%-50s %-50c%n", "N/A", 'N'); // If GTQeObj does not exist, print N/A values
+                }
 
                 
-                System.out.printf("%-50s %-50s %-50s%n",common,official,nativeNameObj);
+                System.out.printf("%-45s %-60s %-280s %-50s %-50s %-50s %-50s %-50s %-50s %-50s %-50s %-50s%n",common,official,nativeNameObj,tld,cca2,ccn3,cca3,
+                		cioc,independent,status,unMember,GTQeObj);
             }
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
           } catch (Exception e) {
             e.printStackTrace();
           }
